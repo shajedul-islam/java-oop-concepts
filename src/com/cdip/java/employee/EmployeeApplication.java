03be393d3c41fd2377cba264f9b8761537abbcd9
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2018-08-31 01:30:34 +0600

    Show how we'd need to call calculateSalary method on each object individually. Imagine what would happen in case of hundreds of such objects.

--- src/com/cdip/java/employee/EmployeeApplication.java
+++ src/com/cdip/java/employee/EmployeeApplication.java
@@ -6,36 +6,42 @@ public class EmployeeApplication {
     public static void main(String[] a) {
 
         FullTimeEmployee john = new FullTimeEmployee();
+        PartTimeEmployee ron = new PartTimeEmployee();
+        ContractualEmployee shaun = new ContractualEmployee();
+        CommissionEmployee bob = new CommissionEmployee();
+        // more objects
+
         john.setId(1);
         john.setName("John");
         john.setBasic(15000);
         john.setHouseRent(5000);
         john.setConveyance(3000);
 
-        john.calculateSalary();
+        ron.setId(2);
+        ron.setName("John");
+        ron.setBasic(12000);
+        ron.setHouseRent(5000);
+        ron.setConveyance(3000);
 
+        shaun.setId(2);
+        shaun.setName("John");
+        shaun.setBasic(12000);
+        shaun.setHouseRent(5000);
+        shaun.setConveyance(3000);
 
-        CommissionEmployee bob = new CommissionEmployee();
-
-        bob.setId(1);
+        bob.setId(5);
         bob.setName("Bob");
         bob.setBasic(9000);
         bob.setHouseRent(5000);
         bob.setConveyance(3000);
         bob.setCommissionRate(5);
 
+        john.calculateSalary();
+        ron.calculateSalary();
+        shaun.calculateSalary();
         bob.calculateSalary();
+        // more method calls
 
-
-
-        Employee someone = new Employee();
-        someone.setId(1);
-        someone.setName("John");
-        someone.setBasic(15000);
-        someone.setHouseRent(5000);
-        someone.setConveyance(3000);
-
-        someone.calculateSalary();
 
     }
 
