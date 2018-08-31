356d8b4a874073b2d7370191f5a73fe0bd1f311d
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2018-08-31 09:39:54 +0600

    Test default value of reference type.

---
+++ src/com/cdip/java/DefaultValueTest.java
@@ -0,0 +1,24 @@
+package com.cdip.java;
+
+public class DefaultValueTest {
+
+    public static void main(String[] a) {
+        MyClass myClass = new MyClass();
+        System.out.println("x: " + myClass.getX());
+    }
+}
+
+class MyClass {
+
+    Integer x;
+
+    int y; // default value 0
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+}
