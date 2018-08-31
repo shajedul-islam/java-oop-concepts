c182837834c37a5bbb639e1fee1aa538cc20a866
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2018-08-31 11:47:51 +0600

    Constructor test. What happens when we declare a constructor in an abstract class?

---
+++ src/com/cdip/java/ConstructorTest.java
@@ -0,0 +1,38 @@
+package com.cdip.java;
+
+
+abstract class Employee {
+
+    static int noOfEmployees;
+
+
+    //TODO: What happens when we declare a constructor in an abstract class?
+    public Employee() {
+        noOfEmployees++;
+        System.out.println("super class: " + noOfEmployees);
+    }
+
+
+}
+
+class FullTimeEmployee extends Employee {
+    public FullTimeEmployee() {
+        super();
+        System.out.println("sub class");
+    }
+
+    /*public FullTimeEmployee(int x) {
+        System.out.println("sub class with argument");
+    }*/
+}
+
+public class ConstructorTest {
+    public static void main(String[] a) {
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee();
+
+        FullTimeEmployee fullTimeEmployee1 = new FullTimeEmployee();
+        FullTimeEmployee fullTimeEmployee2 = new FullTimeEmployee();
+
+        //FullTimeEmployee subClass1 = new FullTimeEmployee(5);
+    }
+}
